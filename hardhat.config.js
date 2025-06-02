3aa4b7a85ca6792c03af2e92af013a4f8cc6cef6
OCOS UK <support@ocos.io>
2025-06-02 04:32:59 +0400

    hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    shasta: {
+      url: process.env.SHASTA_RPC, // TRON testnet HTTP RPC gateway (if EVM-compatible)
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    mainnet: {
+      url: process.env.MAINNET_RPC || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || ""
+  }
+};
